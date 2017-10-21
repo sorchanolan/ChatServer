558c7b54d4e455d6c45c9564ff2db351fcd575ed
sorchanolan <32638110+sorchanolan@users.noreply.github.com>
2017-10-21 18:32:26 +0100

    New clients and chatrooms stored

--- src/main/java/com/company/sorchanolan/ChatServer.java
+++ src/main/java/com/company/sorchanolan/ChatServer.java
@@ -5,51 +5,70 @@ import org.json.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class ChatServer {
   private static int PORT = 6789;
-  private static long idCounter = 1;
+  private static int idCounter = 1;
 
   public static void main(String argv[]) throws Exception {
     ServerSocket welcomeSocket = new ServerSocket(PORT);
     List<Client> clients = new ArrayList<>();
     List<Chatroom> chatrooms = new ArrayList<>();
-    Map<Integer, String> joinIdToClientName = new HashMap<>();
+    Map<String, Integer> clientNameToJoinId = new HashMap<>();
 
     while (true) {
       System.out.println("Begin Comms");
       Socket conSocket = welcomeSocket.accept();
 
-      BufferedReader inFromClient = new
-          BufferedReader(new
-          InputStreamReader(
-          conSocket.getInputStream()));
-
-      DataOutputStream outToClient = new
-          DataOutputStream(conSocket.getOutputStream());
+      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(conSocket.getInputStream()));
+      DataOutputStream outToClient = new DataOutputStream(conSocket.getOutputStream());
 
       String clientMessage = inFromClient.readLine();
       System.out.println(clientMessage);
 
       JoinRequest joinRequest = getJoinRequest(clientMessage);
 
-      String joinResponse = createJoinResponse(joinRequest.getChatroomName()).toString();
+      Client client = new Client(joinRequest.getClientName());
+      if (!clients.contains(client)) {
+        clients.add(client);
+        clientNameToJoinId.put(client.getName(), createID());
+      } else {
+        System.out.println("Client " + client.getName() + " already in chat system");
+      }
+
+      Optional<Chatroom> maybeChatroom = chatrooms.stream().filter(chatroom -> chatroom.getName().equals(joinRequest.getChatroomName())).findFirst();
+      if (!maybeChatroom.isPresent()) {
+        Chatroom chatroom = new Chatroom();
+        chatroom.setName(joinRequest.getChatroomName());
+        chatroom.setRoomRef(createID());
+        chatroom.setPort(PORT + 1);
+        chatroom.setIpAddress("");
+        chatroom.addClient(clientNameToJoinId.get(client.getName()));
+        chatrooms.add(chatroom);
+        System.out.println("Chatroom " + chatroom.getName() + "added");
+      } else {
+        maybeChatroom.get().addClient(clientNameToJoinId.get(client.getName()));
+        System.out.println("Client " + client.getName() + " added to chatroom " + maybeChatroom.get().getName());
+      }
+
+      String joinResponse = createJoinResponse(joinRequest.getChatroomName(), clientNameToJoinId.get(client.getName()).toString()).toString();
       System.out.println(joinResponse);
       outToClient.writeBytes(joinResponse + "\n");
     }
   }
 
-  private static synchronized String createID() {
-    return String.valueOf(idCounter++);
+  private static synchronized int createID() {
+    return idCounter++;
   }
 
-  private static JSONObject createJoinResponse(String chatroomName) {
+  private static JSONObject createJoinResponse(String chatroomName, String joinId) {
     return new JSONObject()
         .put("JOINED_CHATROOM", chatroomName)
         .put("SERVER_IP", "localhost")
         .put("PORT", PORT)
         .put("ROOM_REF", createID())
-        .put("JOIN_ID", createID());
+        .put("JOIN_ID", joinId);
     }
 
     private static JoinRequest getJoinRequest(String input) {
--- src/main/java/com/company/sorchanolan/Client.java
+++ src/main/java/com/company/sorchanolan/Client.java
@@ -7,6 +7,10 @@ public class Client {
     this.name = "";
   }
 
+  public Client(String name) {
+    this.name = name;
+  }
+
   public String getName() {
     return name;
   }
